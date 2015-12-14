c6229b0f1fc0c17cb2b0e1336095dcb8f707fe0e
livoras <livoras@163.com>
2015-12-14 21:44:33 +0800

    htmlparser2

--- test/h2v.spec.js
+++ test/h2v.spec.js
@@ -10,12 +10,12 @@ jsdom()
 
 describe('Make virtual-dom from html', function () {
   it('Making a single root.', function () {
-    var el = h2v('<ul></ul>').vdom
+    var el = h2v('<ul></ul>')
     el.tagName.should.be.equal('ul')
   })
 
   it('If has multiple roots, make a div wrap them all.', function () {
-    var el = h2v('<ul></ul><p></p>').vdom
+    var el = h2v('<ul></ul><p></p>')
     el.tagName.should.be.equal('div')
   })
 
@@ -27,7 +27,7 @@ describe('Make virtual-dom from html', function () {
     ]
     for (var i = 0, len = htmls.length; i < len; i++) {
       var htmlString = htmls[i]
-      var el = h2v(htmlString).vdom
+      var el = h2v(htmlString)
       var dom = el.render()
       var div = document.createElement('div')
       div.appendChild(dom)
@@ -36,33 +36,5 @@ describe('Make virtual-dom from html', function () {
       domString.length.should.be.equal(htmlString.length)
       domString.should.be.equal(htmlString)
     }
-  })
-
-  it('Test textContent.', function () {
-    var vd = h2v.toVirtualDOM({
-      nodeType: 1,
-      tagName: 'div',
-      attributes: [],
-      style: {},
-      childNodes: [{
-        nodeType: 3,
-        textContent: 'This is a string'
-      }]
-    })
-    vd.children[0].should.be.equal('This is a string')
-  })
-
-  it('Test nodeValue for IE.', function () {
-    var vd = h2v.toVirtualDOM({
-      nodeType: 1,
-      tagName: 'div',
-      attributes: [],
-      style: {},
-      childNodes: [{
-        nodeType: 3,
-        nodeValue: 'This is a string'
-      }]
-    })
-    vd.children[0].should.be.equal('This is a string')
   })
 })
